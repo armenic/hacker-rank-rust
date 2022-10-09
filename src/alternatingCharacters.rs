60600df851c94ba69fb4694de3148dccf540f88f
Edgar Manukyan <edgar.manukyan@roche.com>
2022-10-09 13:19:39 -0400

    first

---
+++ src/alternatingCharacters.rs
@@ -0,0 +1,21 @@
+
+fn alternatingCharacters(s: &str) -> i32 {
+    let mut stack: Vec<char> = Vec::new();
+    let l_original = s.len() as i32;
+
+    for i in s.chars() {
+        if stack.len() > 0 {
+            let last_char = stack[stack.len() - 1];
+            if last_char != i {
+                stack.push(i)
+            }
+        } else {
+            stack.push(i)
+        }
+    }
+    let l_stack = stack.len() as i32;
+    println!("The stack is {:?}", stack);
+    println!("Original length is {l_original}");
+    println!("Stack length is {l_stack}");
+    l_original - l_stack
+}
