992b3bdb8d120c6a9997894c715a848833c1e3c4
Edgar Manukyan <edgar.manukyan@roche.com>
2022-10-16 13:29:02 -0400

    u

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 fn main() {
-    assert_eq!(4, theLoveLetterMystery("abcd"));
-    assert_eq!(2, theLoveLetterMystery("abc"));
-    assert_eq!(2, theLoveLetterMystery("cde"));
-    assert_eq!(0, theLoveLetterMystery("abcba"));
-    assert_eq!(2, theLoveLetterMystery("cba"));
+    assert_eq!(44, marcsCakewalk(&[5, 10, 7]));
+    assert_eq!(11, marcsCakewalk(&[1, 3, 2]));
+    assert_eq!(79, marcsCakewalk(&[7, 4, 9, 6]));
+    assert_eq!(
+        59715404338867,
+        marcsCakewalk(&[
+            819, 701, 578, 403, 50, 400, 983, 665, 510, 523, 696, 532, 51, 449, 333, 234, 958, 460,
+            277, 347, 950, 53, 123, 227, 646, 190, 938, 61, 409, 110, 61, 178, 659, 989, 625, 237,
+            944, 550, 954, 439
+        ])
+    );
 
     println!("Success!")
 }
 
 #[allow(non_snake_case)]
-fn theLoveLetterMystery(s: &str) -> i32 {
-    let mut ans: i32 = 0;
-    let bts = s.bytes();
-    let mut stack: Vec<i32> = Vec::new();
-    for b in bts {
-        stack.push(b as i32)
-    }
-    // println!("{:?}", stack);
-
-    let l = stack.len();
-    // let is_even = l % 2 == 0;
-    let n_items = (l as f32 / 2.0).floor() as i32;
-
-    for i in 0..n_items {
-        let lhs = stack[i as usize];
-        let rhs = stack[stack.len() - i as usize - 1];
-        let diff = (lhs as i32 - rhs as i32).abs();
-        ans = ans + diff;
-        // println!("{:?}", lhs);
-        // println!("{:?}", rhs);
-        // println!("{:?}", ans);
+fn marcsCakewalk(calorie: &[i32]) -> i64 {
+    let base: i64 = 2;
+    let mut new_cal: Vec<i64> = Vec::new();
+    for c in calorie {
+        new_cal.push(c.clone() as i64);
     }
 
-    ans
+    // Need reverse sorting, i.e. descending order
+    new_cal.sort();
+    new_cal.reverse();
+
+    let miles: i64 = new_cal
+        .iter()
+        .enumerate()
+        .map(|pair| base.pow(pair.0 as u32) * pair.1)
+        .sum();
+
+    miles
 }
